f9b3c3ae92f3ea64773fa8ab133b1e937e05c925
ri.gayfullin <ri.gayfullin@tensor.ru>
2023-10-22 10:50:12 +0300

    Initial Commit

---
+++ task8/task8App.swift
@@ -0,0 +1,17 @@
+//
+//  task8App.swift
+//  task8
+//
+//  Created by Руслан Гайфуллин on 22.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct task8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
